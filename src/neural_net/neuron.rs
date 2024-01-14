21231ea00284771ebb1f2d32738ad81d3ff45240
mo-mcnabb <morgan.mcnabb.engineer@gmail.com>
2024-01-14 14:05:35 -0500

    changed variable name

--- src/neural_net/neuron.rs
+++ src/neural_net/neuron.rs
@@ -4,14 +4,13 @@ pub struct Neuron {
 }
 
 impl Neuron {
-    pub fn new(num_weights: usize) -> Neuron {
+    pub fn new(num_neurons_in_next_layer: usize) -> Neuron {
         let mut rng = rand::thread_rng();
-        let weights: Vec<f64> = (0..100).map(|_| rng.gen()).collect(); 
+        let weights: Vec<f64> = (0..num_neroun_in_next_layer).map(|_| rng.gen()).collect(); 
         
         Neuron {
             weights: weights,
         }
-        
     }
 }
 
